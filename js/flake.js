161ed801d7df2b3bb1f8c4322689c848b457beac
Christina Kiesz <ckiesz@gmail.com>
2018-01-15 10:16:53 -0800

    Start separate function for wind drift

--- js/flake.js
+++ js/flake.js
@@ -41,6 +41,33 @@ function Flake (x, y, r, d, a, i, ctx) {
 
   };
 
+  this.blizzard = function () {
+    this.a += 0.01;
+    // this.y += Math.cos( this.a + this.d) + 1 + this.r/2;
+    this.y += Math.cos( this.a + this.d ) + 0.1 + this.r/2;
+    this.x += Math.sin( this.a + this.d ) * 1;
+
+    if(this.x > innerWidth + 5 || this.x < -5 || this.y > innerHeight) {
+      if( this.i % 10 > 0 ) {
+        this.x = Math.random()*innerWidth;
+        this.y = -10;
+      } else {
+        //exiting from the right
+        if (Math.sin(this.a) > 0) {
+          //enter from the left
+          this.x = -5;
+          this.y = Math.random()*innerHeight;
+        } else {
+          //enter from the right
+          this.x = innerWidth + 5;
+          this.y = Math.random()*innerHeight;
+        }
+      }
+    }
+
+
+  };
+
   this.snow = function () {
     this.draw();
     this.update();
